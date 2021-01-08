be550a83b8aeb81fcaa373271fb810b59013f46f
q1lon <keyron.zhang@gmail.com>
2021-01-08 09:47:07 +0800

    整理到文件夹

--- binarySearch.c
+++ binarySearch.c
@@ -1,42 +1,45 @@
 /*
  * @Author: q1lon<keyron.zhang@gmail.com>
  * @Date: 2020-11-24 11:09:00
- * @LastEditTime: 2020-12-30 11:26:22
+ * @LastEditTime: 2021-01-07 20:13:55
  * @FilePath: /frist/binarySearch.c
  */
 #include <stdio.h>
 /* 函数声明 */
-int binarySearch(int *nums[], int target);
+int binarySearch(int nums[], size_t i, int target);
 
 int main()
 {
-    int arr[] = {0, 2, 3, 4, 6, 8, 9, 10};
+    int arr[] = {2, 3, 4, 6, 8, 9, 10};
     int n;
-    // int right = sizeof(arr) / sizeof(*arr);
-    // printf("%d\n",right);
-    n = binarySearch(*arr, 2);
-
-    // printf("%d\n", n);
+    size_t len = sizeof(arr) / sizeof(int);
+    n = binarySearch(arr, len, 10);
+    printf("%d\n", n);
     return 0;
 }
 
-//二分查找
-int binarySearch(int *nums[], int target)
+/**
+ * @description: 二分查找
+ * @param {int []} nums 有序数组
+ * @param {size_t} len 有序数组长度
+ * @param {int} target 要定位的数据
+ * @return {*} 返回数组的位置
+ */
+int binarySearch(int nums[], size_t len, int target)
 {
     int left = 0;
-    int tem[]={} ; // 注意
-    
-    int right = sizeof(tem) / sizeof(*tem);
-    printf("%d\n",right);
-    // while (left <= right)
-    // { // 注意
-    //     int mid = (right + left) / 2;
-    //     if (nums[mid] == target)
-    //         return mid;
-    //     else if (nums[mid] < target)
-    //         left = mid + 1; // 注意
-    //     else if (nums[mid] > target)
-    //         right = mid - 1; // 注意
-    // }
+    int right = len;
+    int min = (left + right) / 2;
+
+    while (left <= right)
+    { // 注意
+        int mid = (right + left) / 2;
+        if (nums[mid] == target)
+            return mid + 1;
+        else if (nums[mid] < target)
+            left = mid + 1; // 如果查找的值大于中间位置的值 说明要查找的值在中间值的右侧
+        else if (nums[mid] > target)
+            right = mid - 1;
+    }
     return -1;
 }
